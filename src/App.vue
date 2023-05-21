67995088578481bdcd27ea85dd38c97b77238c46
Carlo Negroni <c.negroni01@ufromail.cl>
2023-05-21 16:40:44 -0400

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+  <Autos/>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Autos from './components/Autos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Autos
   }
 }
 </script>
---
+++ src/components/Autos.vue
@@ -0,0 +1,33 @@
+<template>
+
+<div v-for="auto in autos" :key="auto.index">
+{{ auto }}
+</div>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default{
+    name:'Autos_A',
+    data(){
+        return{
+            autos:[]
+        }
+    },
+
+
+    mounted(){
+        let vue=this;
+        axios.get("http://localhost:8080/automoviles/listar").then(function(response){
+            vue.autos=response.data;
+            console.log(vue.autos)
+
+
+        })
+    }
+
+}
+
+</script>
